173a746a3e0af85b1bca5a4fbad10e7ec7d3a334
David Fertitta <Davidj.fertitta@gmail.com>
2023-08-01 08:29:46 -0600

    .env file for tokens and what not

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -7,7 +7,7 @@ function NotFound() {
         <div className='hero'>
             <div className='text-center hero-content'>
                 <div className='max-w-lg'>
-                    <h1 className='text-8xl.font-bold.mb-8'>OOOPSIEEEES</h1>
+                    <h1 className='text-8xl.font-bold.mb-8'>Oh, man. Bummer.</h1>
                     <p className='text-5xl mb-8'>
                         404 - Yo, we can't find that shit anywhere!
                     </p>
