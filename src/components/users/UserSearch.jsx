a0b735cbd07b896fe8c1a5dbd6f92e0f679070f6
David Fertitta <Davidj.fertitta@gmail.com>
2023-08-09 11:53:45 -0600

    actions file created. refactoring

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,15 +1,16 @@
 import React, { useState, useContext } from 'react';
 import GithubContext from '../../context/github/githubContext';
 import AlertContext from '../../context/alert/AlertContext';
+import { searchUsers } from '../../context/github/GithubActions';
 
 function UserSearch() {
     const [text, setText] = useState('');
 
-    const { users, searchUsers, clearUsers } = useContext(GithubContext);
+    const { users, dispatch } = useContext(GithubContext);
     const { setAlert } = useContext(AlertContext);
 
     const handleChange = (e) => setText(e.target.value);
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
         if (text === '') {
             setAlert(
@@ -17,7 +18,9 @@ function UserSearch() {
                 'error'
             );
         } else {
-            searchUsers(text);
+            dispatch({ type: 'SET_LOADING' });
+            const users = await searchUsers(text);
+            dispatch({ type: 'GET_USERS', payload: users });
             setText('');
         }
     };
@@ -47,7 +50,7 @@ function UserSearch() {
             {users && users.length > 0 && (
                 <div>
                     <button
-                        onClick={clearUsers}
+                        onClick={() => dispatch({ type: 'CLEAR_USERS' })}
                         className='btn btn-ghost btn-lg'>
                         Clear
                     </button>
